f089e43803144d524d4f17daf5508bb3911173bd
ProtacioUno <enervinluan01@gmail.com>
2024-03-27 17:46:22 +0800

    001 fetching again

---
+++ sampol.js
@@ -0,0 +1,71 @@
+// const fs = require('fs');
+
+// // Read the JSON file
+// fs.readFile('sampol.json', 'utf8', (err, jsonString) => {
+//     if (err) {
+//         console.log('Error reading file:', err);
+//         return;
+//     }
+    
+//     try {
+//         // Parse JSON string to JavaScript object
+//         const jsonData = JSON.parse(jsonString);
+        
+//         // Accessing person information
+//         const person1 = jsonData.person1;
+//         const person2 = jsonData.person2;
+//         const person3 = jsonData.person3;
+        
+//         console.log(person1);
+//         console.log(person2);
+//         console.log(person3);
+//     } catch (err) {
+//         console.log('Error parsing JSON string:', err);
+//     }
+// });
+
+
+fetch('sampol.json').then(response => response.json()).then(data=> {
+                // const person1 = data.person1;
+                // const person2 = data.person2;
+                // const person3 = data.person3;
+
+                // console.log(person1);
+                // console.log(person2.name);
+                // console.log(person3);                
+    console.log(data);             
+    console.log(Object.keys(data));             
+    let firstt = Object.keys(data)
+    console.log(typeof data);                     
+    let ul = document.querySelector('ul');      
+    
+    // li.textContent = 'First item' ; 
+    // ul.append(li); 
+    
+    firstt.forEach(titles => {      
+        // console.log(titles);                 
+        let li = document.createElement('li');  
+        li.textContent = titles ;         
+        // console.log(ul.childElementCount)
+        ul.appendChild(li);                  
+    }); 
+
+    // firstt.forEach(x => {
+    //     // let li_  x = "sampoink" ;          
+    // })
+    let numberOfItems = 3 ; 
+
+    // for (let i = 0; i < numberOfItems; i++) {
+        // Create a new list item element
+        // let li = document.createElement('li');
+        
+        // li.textContent = 
+        // li.textContent = 'Item ' + (ul.childElementCount + i + 1); // Adjust the text content as needed
+        
+        // Append the list item to the existing unordered list
+        // li.textContent = 
+        // ul.appendChild(li);
+    // }
+        
+})
+.catch(error => console.error('Error fetching JSON:', error));
--- src/fetch.js
+++ src/fetch.js
@@ -4,22 +4,41 @@ fetch('src/')
 
 fetch('api/philippine_provinces_cities_municipalities_and_barangays_2019v2.json').then(res => res.json()).then(data => {
 
-    let dropdown = document.getElementById('dropdown'); 
+    // let dropdown = document.getElementById('dropdown'); 
     
-    dropdown.innerHTML = '';
+    // dropdown.innerHTML = '';
     
-    data.forEach(item => { 
-        const ell = document.querySelector('ul');
+    
+    console.log(Object.keys(data)); 
+    
+
+    let province = Object.keys(data); 
+    // let ul = document.querySelector('ul#province'); 
+    let ul = document.querySelector('ul#dropdown-menu'); 
+
+    province.forEach(provc =>{
+        let li  = document.createElement('li');
+        let a  = document.createElement('a')                 
+        li.appendChild(a); 
+        a.textContent = provc ; 
+        a.classList.add('dropdown-item') ;
+        ul.appendChild(li);     
+                
+    }) 
+    // data.forEach(item => { 
+    //     const li = document.querySelector('li');
+        
+        
         // const option = document.createElement('option');
     //   option.text = item.name; // Assuming the JSON has a "name" property
     //   option.value = item.id;  // Assuming the JSON has an "id" property
     //   dropdown.appendChild(option);
         // console.log(item.name)
 
-        ell.insertAdjacentHTML('beforeend',`<li>${post.region_name}</li>`)
+        // ell.insertAdjacentHTML('beforeend',`<li>${post.region_name}</li>`)
         
         
-    });
+    // });
     // console.log(data); 
     // console.log(data['NCR']); 
 })
