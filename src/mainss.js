d46925010f88634f9e89febbd0f1e251dcde5f74
ProtacioUno <enervinluan01@gmail.com>
2024-04-19 15:32:33 +0800

    004 check values

--- src/mainss.js
+++ src/mainss.js
@@ -58,23 +58,47 @@ function condog(){
     console.log("selectced +" , condo_ask); 
     let build_name = document.querySelector('#build_name') 
     let unit = document.querySelector('#unit') 
+    let form_6 = document.querySelector('#form_6')
 
     if(condo_ask == 'no'){
+
         build_name.setAttribute('disabled' , 'disabled');
         build_name.setAttribute('value' , '');
         console.log(build_name)
         unit.setAttribute('disabled' , 'disabled');
         unit.setAttribute('value' , '');
         console.log(unit)
+
+        form_6.setAttribute('hidden' , 'hidden')
         // build_nam
     } else {
-         
+        
+     
         build_name.removeAttribute('disabled');
         build_name.removeAttribute('value'); 
         
         unit.removeAttribute('disabled');
         unit.removeAttribute('value');
+        form_6.removeAttribute('hidden');
         console.log(build_name)
         console.log(unit)
     }
+}
+
+
+// var input = document.getElementById("numericInput");
+
+// // Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function(event) {
+//   // Cancel the default action, if needed
+//   event.preventDefault();
+//   // Get the value of the input field
+//   var value = input.value;
+//   // Remove non-numeric characters using regular expression
+//   input.value = value.replace(/\D/g, '');
+// });
+ 
+function validateNumberInput(input) {
+    // Remove non-numeric characters using regular expression
+    input.value = input.value.replace(/\D/g, '');
 }
