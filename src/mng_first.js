4766478356ff5288e3677796a91285c2ca3fddc2
ProtacioUno <enervinluan01@gmail.com>
2024-04-22 16:06:20 +0800

    005 shitness

---
+++ src/mng_first.js
@@ -0,0 +1,26 @@
+
+let n_home = document.querySelector('#home') , 
+    n_contact = document.querySelector('#contact_data') , n_bir = document.querySelector('#bir'); 
+
+// n_home.addEventListener('click' , ()=> {
+//  // console.log('Bait');
+//     n_home.classList.toggle('activated_nav'); 
+//     console.log(n_home); 
+    
+    
+    
+// }) 
+// n_contact.addEventListener('click' , ()=> {
+//     // console.log('syete');
+//     n_contact.classList.toggle('activated_nav'); 
+        
+// }) 
+
+// n_bir.addEventListener('click' , ()=>{
+//     n_bir.classList.toggle('activated_nav'); 
+    
+// })
+
+
+
+
